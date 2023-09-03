5d16d659f12fbdf5948eeeb9ba456e04d4d67118
Ascend Ho <coderhs@foxmail.com>
2023-09-03 20:58:49 +0800

    add lecture

---
+++ Interview Problems/09-Cycle_Detection/CycleDetection.h
@@ -0,0 +1,17 @@
+#ifndef INC_09_CYCLE_DETECTION_CYCLEDETECTION_H
+#define INC_09_CYCLE_DETECTION_CYCLEDETECTION_H
+
+/* Definition for singly-linked list. */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    explicit ListNode(int x) : val(x), next(nullptr) {}
+};
+
+ListNode *detectCycle_Floyd(ListNode *head);
+
+ListNode *detectCycle_HashMap(ListNode *head);
+
+#endif //INC_09_CYCLE_DETECTION_CYCLEDETECTION_H
